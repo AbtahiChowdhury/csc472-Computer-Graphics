6620611b2c4b83738eea97f8bb2a8af9c0e7ac97
eramm805 <eram.manasia@gmail.com>
2020-03-17 15:34:05 -0400

    got first P to work and is now yellow

--- hw2/vshader2a.glsl
+++ hw2/vshader2a.glsl
@@ -1,9 +1,10 @@
-#version 330
+#version 330 //open gl 3.3
 
 in	vec2	a_Position;	// attribute variable
 uniform	mat4	u_Projection;	// uniform variable for passing projection matrix
 void main() 
 {
 	// PUT YOUR CODE HERE
+    gl_PointSize = 3.0;
 	gl_Position = u_Projection * vec4(a_Position, 0, 1);
 }
